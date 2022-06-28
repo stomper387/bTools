53874fa7e42650bfb92f35820607e2e460a9f108
stomper387 <stomper387@gmail.com>
2022-06-29 05:45:43 +1000

    v0.1-1

--- src/me/stomper/btools/Main.java
+++ src/me/stomper/btools/Main.java
@@ -1,22 +1,21 @@
 package me.stomper.btools;
 
-
 import org.bukkit.plugin.java.JavaPlugin;
-
 
 public class Main extends JavaPlugin {
 	
 
-			@Override
-			public void onEnable() {
-			this.getCommand("heal").setExecutor(new heal()); 
-			this.getCommand("fly").setExecutor(new fly());
-			this.getCommand("kick").setExecutor(new kick());
-			this.getCommand("ban").setExecutor(new ban());
-			this.getCommand("echest").setExecutor(new echest());
-			this.getCommand("gmc").setExecutor(new gmc());
-			this.getCommand("gms").setExecutor(new gms());
-			this.getCommand("gamemode").setExecutor(new gamemode());
-			this.getCommand("broadcast").setExecutor(new broadcast());
-			}
+	@Override
+	public void onEnable() {
+	this.getCommand("heal").setExecutor(new heal()); 
+	this.getCommand("fly").setExecutor(new fly());
+	this.getCommand("kick").setExecutor(new kick());
+	this.getCommand("ban").setExecutor(new ban());
+	this.getCommand("echest").setExecutor(new echest());
+	this.getCommand("gmc").setExecutor(new gmc());
+	this.getCommand("gms").setExecutor(new gms());
+	this.getCommand("gamemode").setExecutor(new gamemode());
+	this.getCommand("broadcast").setExecutor(new broadcast());
+	this.getCommand("g").setExecutor(new gapple());
+	}
 }
---
+++ src/me/stomper/btools/gapple.java
@@ -0,0 +1,59 @@
+package me.stomper.btools;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class gapple implements CommandExecutor {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("g")) {
+			Player player = (Player) sender;
+			if (!player.hasPermission("btools.gapple")) {
+				player.sendMessage(ChatColor.DARK_RED + "You do not have sufficient permissions");
+				return true;
+			}
+			if (args.length == 0) {
+				player.setHealth(20.0);
+				player.setFoodLevel(20);
+				player.setAbsorptionAmount(20);
+				player.setSaturation(20);
+				player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 6000, 2));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 6000, 1));
+				player.sendMessage(ChatColor.GOLD + "You have been blessed.");
+				return true;
+			}
+			if (args.length == 1) {
+				
+				boolean playerFound = false;
+				
+				for (Player playerToBless : Bukkit.getServer().getOnlinePlayers()) {
+					if(playerToBless.getName().equalsIgnoreCase(args[0])) {
+						playerToBless.setHealth(20.0);
+						playerToBless.setFoodLevel(20);
+						playerToBless.setAbsorptionAmount(20);
+						playerToBless.setSaturation(20);
+						playerToBless.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 6000, 2));
+						playerToBless.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 6000, 1));
+						playerToBless.sendMessage(ChatColor.GOLD + "You have been blessed by " + player.getName());
+						player.sendMessage(ChatColor.GOLD + playerToBless.getName() + " was blessed successfully");
+						playerFound = true;
+						break;
+					}
+				}
+				
+				if (playerFound == false) {
+					player.sendMessage(ChatColor.DARK_RED + args[0] + " was not found");
+				}
+			} else player.sendMessage(ChatColor.DARK_RED + "correct usage: /g <username>");
+			
+			return true;
+		}
+		
+		return true;
+	}
+}
